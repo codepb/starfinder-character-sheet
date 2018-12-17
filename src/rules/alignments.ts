f4fbb818f6dc7a27116c2b36ccf6b2929dbfb436
codepb <paul.j.burgess@outlook.com>
2018-12-17 16:10:21 +0000

    adding more rules

---
+++ src/rules/alignments.ts
@@ -0,0 +1,13 @@
+enum Alignments {
+  lawfulGood,
+  lawfulNeutral,
+  lawfulEvil,
+  neutralGood,
+  neutral,
+  neutralEvil,
+  chaoticGood,
+  chaoticNeutral,
+  chaoticEvil
+}
+
+export default Alignments;
